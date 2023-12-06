46fa7ab9a71860d1031c22b94dd2a5ed5f1531fd
Lukaswyver <lucas_sin5@hotmail.com>
2023-12-06 17:25:34 -0300

    chore: changed services banner

--- src/components/Banner/Servicos.js
+++ src/components/Banner/Servicos.js
@@ -6,14 +6,16 @@ import { motion } from "framer-motion";
 function BannerServicos() {
   return (
     <>
-      <section className="lg:h-[700px] py-[88px] px-3 mb-6 bg-cover bg-bottom bg-no-repeat bg-image-fundo-servicos">
+      {/* lg:h-[760px] */}
+      <section className="lg:h-[480px] py-[88px] px-3 mb-6 bg-cover bg-bottom bg-no-repeat bg-image-fundo-servicos">
         <h3 className="text-white text-4xl font-bold text-center after-bottom opacity-0 animate-slide-up">
-          Serviços
+          Especialistas em:
         </h3>
 
-        <div className="mt-[76px] mb-[90px] grid xm:grid-cols-2 wrapper gap-8 md:gap-4">
+        {/* xm:grid-cols-2 */}
+        <div className="mt-[76px] mb-[90px] grid xm:grid-cols-1 wrapper gap-8 md:gap-4">
             <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.1 }}>
-              <Link href="/servicos/acompanhamento-de-obras" className=" hover:bg-black/10 transition-colors rounded-md p-3 flex gap-6 items-center flex-col justify-center ss:flex-row ss:justify-start space-y-3.5 text-white text-center max-w-[493px] mx-auto">
+              <Link href="/servicos/acompanhamento-de-obras" className=" hover:bg-black/10 transition-colors rounded-md p-3 flex gap-6 items-center flex-col justify-center ss:flex-row ss:justify-start space-y-3.5 text-white text-center max-w-[560px] flex-1 mx-auto">
                 <Image width={86} height={86} alt="agilidade" className="w-[86px] h-[86px]" src="/icons/acompanhamento-de-obras.svg" />
                 <div className="space-y-2.5">
                   <h5 className="font-bold text-[29px] leading-[30px] ss:text-left break-normal xm:break-all lg:break-normal">
@@ -31,7 +33,7 @@ function BannerServicos() {
               </Link>
             </motion.div>
 
-            <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.2 }}>
+            <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.2 }} className="sr-only">
               <Link href="/servicos/manutencao-predial" className="hover:bg-black/10 transition-colors rounded-md p-3 flex gap-6 items-center flex-col justify-center ss:flex-row ss:justify-start space-y-3.5 text-white text-center max-w-[493px] mx-auto">
                 <Image width={83} height={83} alt="confiança" className="w-[83px] h-[83px]" src="/icons/manutencao-predial.svg"/>
                 <div className="space-y-2.5">
@@ -50,7 +52,7 @@ function BannerServicos() {
               </Link>
             </motion.div>
 
-            <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.3 }}>
+            <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.3 }} className="sr-only">
               <Link href="/servicos/projetos" className="hover:bg-black/10 transition-colors rounded-md p-3 flex gap-6 items-center flex-col justify-center ss:flex-row ss:justify-start space-y-3.5 text-white text-center max-w-[493px] mx-auto">
                 <Image width={86} height={95} alt="fazemos bem feito" src="/icons/projetos.svg" className="w-[86px] h-[95px]" />
                 <div className="space-y-2.5">
@@ -69,7 +71,7 @@ function BannerServicos() {
               </Link>
             </motion.div>
 
-            <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.4 }}>
+            <motion.div initial={{ opacity: 0, scale: 0.5 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: 0.4 }} className="sr-only">
               <Link href="/servicos/art"  className="hover:bg-black/10 transition-colors rounded-md p-3 flex gap-6 items-center flex-col justify-center ss:flex-row ss:justify-start space-y-3.5 text-white text-center max-w-[493px] mx-auto">
                 <Image width={75} height={100} alt="segurança" src="/icons/art.svg" className="ss:-mt-4 ss:mb-4 w-[75px] h-[100px]" />
                 <div className="space-y-2.5">
